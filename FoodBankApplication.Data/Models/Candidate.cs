bd38ba6a3b5abc4b5b5e8d9c0796eee3654bd7ca
Thokozani Zakwe <tezakwe@gmail.com>
2024-01-05 19:24:14 +0200

    Candidate class update, HighSchoolGrade and Municipality add

--- FoodBankApplication.Data/Models/Candidate.cs
+++ FoodBankApplication.Data/Models/Candidate.cs
@@ -11,6 +11,16 @@ namespace FoodBankApplication.Data.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string Province { get; set; }
+        public string PostalCode { get; set; }
+        public string ContactNumbers { get; set; }
+        public string Region { get; set; }
+        public string Comment { get; set; }
+        public string Gender { get; set; }
+        public bool IsDisabled { get; set; }
         public string IDNumber { get; set; } = string.Empty;
         public DateTime? DOB { get; set; }
         public bool IsDeleted { get; set; }
@@ -18,5 +28,11 @@ namespace FoodBankApplication.Data.Models
 
         public int StatusId { get; set; }
         public virtual Status Status { get; set; }
+
+        public int HighShcoolGradeId { get; set; }
+        public HighSchoolGrade HighSchoolGrade { get; set; }
+
+        public int MunicipalityId { get; set; }
+        public Municipality Municipality { get; set; }
     }
 }
