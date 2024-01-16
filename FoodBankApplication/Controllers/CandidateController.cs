969e97c7c4fceb81f29bbbb288a69013dc3e59fb
Thokozani Zakwe <tezakwe@gmail.com>
2024-01-16 20:08:35 +0200

    Candidate Added

---
+++ FoodBankApplication/Controllers/CandidateController.cs
@@ -0,0 +1,24 @@
+﻿using FoodBankApplication.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodBankApplication.Controllers
+{
+    public class CandidateController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public int PageSize = 4;
+
+        public CandidateController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index(int pageNumber = 1)
+        {
+            var candidates = _context.Candidates.Where(c => !c.IsDeleted).
+                OrderBy(x => x.Id).Skip((pageNumber - 1) * PageSize).Take(PageSize).ToList();
+            return View(candidates);
+        }
+
+        public IActionResult Add() => View();
+    }
+}
